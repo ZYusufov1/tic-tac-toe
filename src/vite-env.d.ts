e49121dec54719c83e31a3e2cf9995236bb757a9
Zaur Yusufov <zaury8252@gmail.com>
2024-12-27 02:46:42 +0400

    Make main page

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+declare module '*.svg' {
+    import React from 'react'
+    const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>
+    const src: string
+    export { ReactComponent }
+    export default src
+}
